4e28e68cb2a92838b9592269bc5bded4f6c74ff8
dilarakilic <dilaraaklc@gmail.com>
2025-04-17 18:42:59 +0300

    fix app name

--- src/main/java/org/example/routeapp/RouteApplication.java
+++ src/main/java/org/example/routeapp/RouteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RouteAppApplication {
+public class RouteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RouteAppApplication.class, args);
+        SpringApplication.run(RouteApplication.class, args);
     }
 
 }
--- src/test/java/org/example/routeapp/RouteAppApplicationTests.java
+++ src/test/java/org/example/routeapp/RouteAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RouteAppApplicationTests {
+class RouteApplicationTests {
 
     @Test
     void contextLoads() {
